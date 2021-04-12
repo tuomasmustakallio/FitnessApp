988fd4ef3c6b2fa124468f5a361a935a8f06ba73
Jalex28 <joonaaleks@gmail.com>
2021-04-12 18:02:03 +0300

    Eka fragmentti rekisteröinnille

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("test asdfasdfasdfasdf");
+
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Fragment fragment;
+
+                if (view == findViewById(R.id.btn_reg)) {
+                    System.out.println("RegisterNow");
+                }
+
+                fragment = new RegisterFragment();
+
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.screen1,fragment);
+                transaction.commit();
+
+            }
+        };
+
+        Button btn_reg = findViewById(R.id.btn_reg);
+        btn_reg.setOnClickListener(listener);
     }
 }
