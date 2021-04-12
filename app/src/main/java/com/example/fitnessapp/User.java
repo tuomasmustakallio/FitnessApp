364bd2e5a112965b64c53048524ea0f1dc5b25a9
Jalex28 <joonaaleks@gmail.com>
2021-04-12 20:42:20 +0300

    Fragmentti käyttäjälle

---
+++ app/src/main/java/com/example/fitnessapp/User.java
@@ -0,0 +1,11 @@
+package com.example.fitnessapp;
+
+public class User {
+    String username, email, password;
+
+    public User (String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
