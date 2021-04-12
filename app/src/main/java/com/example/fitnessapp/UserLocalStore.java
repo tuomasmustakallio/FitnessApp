57a345bf3b26b08e03dc3f997628b2449a948e59
Jalex28 <joonaaleks@gmail.com>
2021-04-12 22:03:02 +0300

    Lisätty käyttäjä database

---
+++ app/src/main/java/com/example/fitnessapp/UserLocalStore.java
@@ -0,0 +1,42 @@
+package com.example.fitnessapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class UserLocalStore {
+
+    public static final String SP_NAME = "userDetails";
+    SharedPreferences userLocalDatabase;
+
+    public UserLocalStore(Context context) {
+        userLocalDatabase = context.getSharedPreferences(SP_NAME, 0);
+    }
+
+    public void storeUserData(User user){
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putString("username", user.username);
+        spEditor.putString("email", user.email);
+        spEditor.putString("password", user.password);
+        spEditor.commit();
+    }
+
+    public User getLoggedInUser() {
+        String username = userLocalDatabase.getString("username","");
+        String email = userLocalDatabase.getString("email","");
+        String password = userLocalDatabase.getString("password","");
+
+        User storedUser = new User(username, email, password);
+        return storedUser;
+    }
+
+    public void setUserLoggedIn(boolean loggedIn){
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("loggedIn", loggedIn);
+        spEditor.commit();
+    }
+
+    public void clearUserData(){
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.clear();
+    }
+}
