0b6126f8360ae4b271cc0daa2200db54706bd146
tuomasmustakallio <tuoppi38@gmail.com>
2021-04-18 18:24:12 +0300

    Working side bar added to MainActivity.java and activity_main.xml which switches between fragments.

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -1,32 +1,45 @@
 package com.example.fitnessapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        Fragment loginFragment;
-        loginFragment = new LoginFragment();
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        transaction.replace(R.id.screen1,loginFragment);
-        transaction.commit();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        View.OnClickListener listener = new View.OnClickListener(){
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LoginFragment()).commit();
+        }
+        /*View.OnClickListener listener = new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 FragmentManager manager = getSupportFragmentManager();
@@ -38,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 fragment = new RegisterFragment();
-                transaction.replace(R.id.screen1,fragment);
+                transaction.replace(R.id.toolbar,fragment);
                 transaction.commit();
 
             }
@@ -56,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 fragment1 = new DataInput();
-                transaction.replace(R.id.screen1,fragment1);
+                transaction.replace(R.id.toolbar,fragment1);
                 transaction.commit();
 
             }
@@ -67,6 +80,40 @@ public class MainActivity extends AppCompatActivity {
         inputbtn.setOnClickListener(listener1);
 
         Button btn_reg = findViewById(R.id.btn_reg);
-        btn_reg.setOnClickListener(listener);
+        btn_reg.setOnClickListener(listener);*/
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_login_register:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LoginFragment()).commit();
+                break;
+            case R.id.nav_account:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DataInput()).commit();
+                break;
+            case R.id.nav_input:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DataInput()).commit();
+                //TODO ADD PROGRESS INPUT
+                break;
+            case R.id.nav_output:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DataInput()).commit();
+                //TODO ADD SHOW DATA
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
