ca7f07565ef417f24f0ba246d73abd1b5e964365
elmurmeli <altti.vaatainen@gmail.com>
2021-04-19 20:59:04 +0300

    DatabaseHelper class first commit

--- app/src/main/java/com/example/fitnessapp/Database.java
+++ app/src/main/java/com/example/fitnessapp/Database.java
@@ -2,16 +2,16 @@ package com.example.fitnessapp;
 
 public class Database {
 
-        int age;
-        String gender;
-        String height;
-        int weight;
+    int age;
+    String gender;
+    int height;
+    int weight;
 
-        public DataBase(){
+    public Database(){
 
     }
 
-    public DataBase(int age, String gender, String height, int weight){
+    public Database(int age, String gender, int height, int weight){
         this.age = age;
         this.gender = gender;
         this.height = height;
@@ -23,18 +23,33 @@ public class Database {
             return age;
     }
 
-    public int setAge(int age){
-        return age;
+    public void setAge(int age){
+            this.age = age;
     }
 
     public String getGender(){
             return gender;
     }
 
-    public String setGender(String gender){
-        this.gender = gender;
+    public void setGender(String gender){
+            this.gender = gender;
     }
 
+    public int getHeight(){
+            return height;
+    }
+
+    public void setHeight(){
+            this.height = height;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public void setWeight(){
+        this.weight = weight;
+    }
 
 
 }
---
+++ app/src/main/java/com/example/fitnessapp/DatabaseHelper.java
@@ -0,0 +1,33 @@
+package com.example.fitnessapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "databaseManager";
+    private static final String TABLE_DATABASE = "database";
+    private static final String KEY_AGE = "age";
+    private static final String KEY_GENDER = "gender";
+    private static final String KEY_HEIGHT = "height";
+    private static final String KEY_WEIGHT = "weight";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
