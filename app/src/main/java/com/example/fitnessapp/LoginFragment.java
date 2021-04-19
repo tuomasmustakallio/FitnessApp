c482c83f23cc6db1a1bbab5e6567319f652d2f05
tuomasmustakallio <tuoppi38@gmail.com>
2021-04-19 22:52:16 +0300

    LoginFragment.java added login button and the logic for it.

--- app/src/main/java/com/example/fitnessapp/LoginFragment.java
+++ app/src/main/java/com/example/fitnessapp/LoginFragment.java
@@ -1,9 +1,13 @@
 package com.example.fitnessapp;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,12 +15,61 @@ import androidx.fragment.app.Fragment;
 
 
 public class LoginFragment extends Fragment {
+
+    EditText etUsername, etPassword;
+    String username, password;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        etUsername = getView().findViewById(R.id.etUsername);
+        etPassword = getView().findViewById(R.id.etPassword);
+
+        etUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                username = etUsername.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        etPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                password = etPassword.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         return inflater.inflate(R.layout.login_layout, container, false);
     }
+
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    public void tryLogin(){
+        //TODO CHECK IF USERNAME LEGIT
+        // if  username is not recognized Toast.makeText(getContext(), "Invalid username", Toast.LENGTH_SHORT).show();
+        //TODO CHECK IF PASSWORD MATHCES WITH checkPassword(username, password) == true
+        // if  password is not recognized Toast.makeText(getContext(), "Invalid password", Toast.LENGTH_SHORT).show();
     }
 }
