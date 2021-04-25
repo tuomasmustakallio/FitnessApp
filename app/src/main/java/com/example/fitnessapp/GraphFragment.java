e205ea055200f8e4d640386c0651f96973e5363a
elmurmeli <altti.vaatainen@gmail.com>
2021-04-25 21:40:26 +0300

    Trying to get the graph work again and again

--- app/src/main/java/com/example/fitnessapp/GraphFragment.java
+++ app/src/main/java/com/example/fitnessapp/GraphFragment.java
@@ -9,30 +9,51 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.w3c.dom.Entity;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class GraphFragment extends Fragment {
 
-    private LineGraphSeries<DataPoint> series1;
+    LineChart mpLineChart;
 
 
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        double y,x;
-        x=-0.5;
-        View view = inflater.inflate(R.layout.graph_layout, container, false);
-        GraphView graph = (GraphView) getView().findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
-        });
-        graph.addSeries(series);
-        return view;
+
+        mpLineChart=(LineChart) getView().findViewById(R.id.line_chart);
+        LineDataSet lineDataSet1 = new LineDataSet(dataValues1(),"Data Set 1");
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet1);
+
+        LineData data = new LineData(dataSets);
+        mpLineChart.setData(data);
+        mpLineChart.invalidate();
+
+        return inflater.inflate(R.layout.login_layout, container, false);
+    }
+
+    private ArrayList<Entry> dataValues1(){
+        ArrayList<Entry> dataVals = new ArrayList<Entry>();
+        dataVals.add(new Entry(0,20));
+        dataVals.add(new Entry(1,24));
+        dataVals.add(new Entry(2,2));
+        dataVals.add(new Entry(3,10));
+        dataVals.add(new Entry(4,28));
+
+        return dataVals;
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
